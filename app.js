4f204e6ef3bad53386a15b510b6db05d8bb507b8
Marcel <marcelgrygar7@gmail.com>
2020-06-12 23:37:19 +0200

    Because of crash and same error express.Router(); is not a function

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const Joi = require('joi');
+
 var indexRouter = require('./routes/index');
 
 
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+
 // Všechny media
 let all_media = [];
-
-app.use(cors());
 
 // Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 
 //Všechny media
 app.get('/api/v1/media', async (req,res)=> {
@@ -84,18 +96,18 @@ app.put('/api/v1/media/:guid', async (req,res)=> {
   media.release_year = req.body.release_year;
   res.send(media);
 
-/*
- const guid = req.params.guid;
-  const newMedia = req.body;
+  /*
+   const guid = req.params.guid;
+    const newMedia = req.body;
 
-for (let i = 0; i < all_media.length; i++) {
-  let media = all_media[i]
-  if (media.guid === guid) {
-    all_media[i] = newMedia;
-  }else{
-    res.status(404).send('Media record was not found');
-  }
-}*/
+  for (let i = 0; i < all_media.length; i++) {
+    let media = all_media[i]
+    if (media.guid === guid) {
+      all_media[i] = newMedia;
+    }else{
+      res.status(404).send('Media record was not found');
+    }
+  }*/
 });
 
 function validateMedia(media){
@@ -103,8 +115,8 @@ function validateMedia(media){
     title:Joi.string().required(),
     type:Joi.string().required(),
     kind:Joi.string().required(),
-    number_of_discs:Joi.integer.required(),
-    release_year:Joi.integer.required(),
+    number_of_discs:Joi.integer,
+    release_year:Joi.integer,
   }
   return Joi.validate(media, schema);
 }
@@ -125,14 +137,6 @@ app.delete('/api/v1/media/:guid', async (req,res)=> {
   res.send('Media is deleted');
 });
 
-
-/*
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -148,5 +152,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-*/
+
 module.exports = app;
